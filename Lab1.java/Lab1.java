224ec75253afdcd804ecc0b64b92b78f81181188
Tasfia Zaman Tanha <tztanha@utas.edu.au>
2026-03-10 14:27:31 +1100

    First commit Hello World program

---
+++ Lab1.java/Lab1.java
@@ -0,0 +1,13 @@
+/**
+ * KIT107 Lab 1
+ * @author Tasfia Tanha
+ * @version Week 1
+ */
+
+public class Lab1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
